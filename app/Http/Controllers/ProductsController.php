830225dfdfaa7ce22d9b6d6fc1a0c1411b58e2c3
amanatsu <mk0874@yahoo.co.jp>
2022-07-28 16:29:15 +0900

    make product index

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
@@ -13,7 +14,13 @@ class ProductsController extends Controller
      */
     public function index()
     {
-        //
+        // 商品一覧
+        $products = Product::all();
+
+        return view(
+            'product.index',
+            ['products' => $products]
+        );
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 商品マスタ ルーティング一覧(showは使わない)
+Route::resource('product', ProductsController::class, ['except' => ['show']]);
